6091b596fe9d519164cd2b375b09669818367848
exttNd <mcv3363334510@163.com>
2021-04-24 22:31:54 +0800

    "test"

---
+++ README.md
@@ -0,0 +1,3 @@
+在idea中建立一个kotlin demo
+
+cesihiyixa
---
+++ src/demo/Hello.kt
@@ -0,0 +1,10 @@
+package demo
+
+/**
+ * @ Description：
+ * Created by Stone on 7/8/18.
+ * @ Time 7/8/18 10:27 PM
+ */
+fun main(args: Array<String>) {
+    println("Stone")
+}
